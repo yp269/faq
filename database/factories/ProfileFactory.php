53946baed789b75aee61f7ec010545f389cb3ab9
Yi <yp269@njit.edu>
2019-05-14 18:05:32 -0400

    Edited User Profile migration, model, and updated the user seeder to create profiles.

---
+++ database/factories/ProfileFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use Faker\Generator as Faker;
+$factory->define(App\Profile::class, function (Faker $faker) {
+    return [
+        'fname' => $faker->firstName,
+        'lname' => $faker->lastName,
+        'body' => $faker->paragraph($nbSentences = 3, $variableNbSentences = true),
+    ];
+});
